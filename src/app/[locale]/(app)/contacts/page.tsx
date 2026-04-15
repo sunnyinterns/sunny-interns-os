e12f94c3ff7f7096510a1ddcafab427517c77113
Sidney Ruby <sidney.ruby@gmail.com>
2026-04-15 17:14:40 +0800

    fix: import Link ajouté dans contacts/page.tsx, TS propre

--- src/app/[locale]/(app)/contacts/page.tsx
+++ src/app/[locale]/(app)/contacts/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import { SearchableSelect, type SearchableSelectItem } from '@/components/ui/SearchableSelect'
 
 import { useEffect, useState } from 'react'
