5186c0757f7eac57cdaf08c2ce260bc09f83d50e
Sidney Ruby <sidney.ruby@gmail.com>
2026-04-16 13:00:56 +0800

    feat: sentry-example-page pour test monitoring

---
+++ src/app/sentry-example-page/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import * as Sentry from '@sentry/nextjs'
+
+export default function SentryPage() {
+  return (
+    <div style={{padding:'40px',fontFamily:'sans-serif'}}>
+      <h1>Sentry test</h1>
+      <button onClick={() => { throw new Error('Sentry test error - Sunny Interns OS') }}>
+        Trigger test error
+      </button>
+      <button style={{marginLeft:'16px'}} onClick={() => Sentry.captureMessage('Sentry test message OK')}>
+        Send test message
+      </button>
+    </div>
+  )
+}
